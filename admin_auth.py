fa54a6a8706fad32c28570d3115def6e3493e204
avyuteck-lgtm <avyuteck@gmail.com>
2026-03-11 12:03:23 +0530

    Update admin_auth.py

--- admin_auth.py
+++ admin_auth.py
@@ -2,10 +2,11 @@ import json
 
 def authenticate(username, password):
 
-    with open("admin_config.json") as f:
+    with open("users.json") as f:
         admin = json.load(f)
 
-    if username == admin["username"] and password == admin["password"]:
-        return True
+        if username in users:
+            if password == users[username]["password"]:
+                return True
 
     return False
