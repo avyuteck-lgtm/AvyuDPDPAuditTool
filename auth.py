2175a93990acba2196fd786d637232036ac12006
avyuteck-lgtm <avyuteck@gmail.com>
2026-03-11 15:12:54 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,10 +3,10 @@ import json
 def authenticate(username, password):
 
     with open("users.json") as f:
-        admin = json.load(f)
+        users = json.load(f)
 
-        if username in users:
-            if password == users[username]["password"]:
-                return True
+    if username in users:
+        if password == users[username]["password"]:
+            return True
 
     return False
